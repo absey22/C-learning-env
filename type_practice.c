050fabee358884debb4b423c29f158eba74513f9
absey22 <abseymour@gmail.com>
2020-11-15 18:32:51 +0100

    practice with datatypes in C

---
+++ type_practice.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+  char ch;
+  
+  ch='x';
+  printf("%c \n",ch);
+
+  int age;
+
+  age=30;
+  printf("That guy is %d years old.\n",age);
+
+  float pi;
+
+  pi = 22.0/7.0;
+
+  printf("Pie is %f \n",pi);
+  
+  
+  return(0);
+
+}
