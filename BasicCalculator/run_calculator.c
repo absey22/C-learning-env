da362e3bae19cb71feb641afff106dcf06f6b8d2
absey22 <abseymour@gmail.com>
2020-11-16 16:08:55 +0100

    calculator does IO, and 4 operation types.

--- BasicCalculator/run_calculator.c
+++ BasicCalculator/run_calculator.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "calculator_functions.h"
 
 
@@ -16,47 +17,55 @@ int main()
 {
   
   float x1,x2;
-  char operationtype[4];
+  char operationtype[4+1];
  
 
-  printf("Desired Operation? (options: mult/divi/addi/subt) \n");
-  scanf("%c",&operationtype)
-
-    // if(sizeof(operationtype) != 4)
-    //{}
-    while(operationtype != "mult" || operationtype != "divi" or operationtype != "addi" or operationtype != "subt")
-      {
-	printf("Try again with oepration type:\n");
-	scanf("%c",&operationtype)
-      }
-   
   printf("First Number: ");
   scanf("%f",&x1);
 
   printf("Second Number: ");
   scanf("%f",&x2);
-
-
   
-  switch(operationtype)
-    {
-    case "mult":
-      printf("%f",mult(x1,x2));
-      break;
-    case "divi":
-      printf("%f",divi(x1,x2));
-      break;
-    case "addi":
-      printf("%f",addi(x1,x2));
-      break;
-    case "subt":
-      printf("%f",subt(x1,x2));
-      break;
-    default :
-      printf("Invalid option");
-    }
   
+  printf("Desired Operation? (options: mult/divi/addi/subt) \n");
+  scanf("%s",operationtype);
+
+  /*
+
+  while(strcmp(operationtype,"mult")==1 || strcmp(operationtype,"divi")==1 || strcmp(operationtype,"addi")==1 || strcmp(operationtype,"subt")==1 )
+      {
+	printf("Invalid Operation. Try again with operation type:\n");
+	scanf("%s",operationtype);
+      }
+  */
+  printf("Will now do: %f  %s  %f. \n",x1, operationtype,x2);
+    
+   
+   
+    
+   if(strcmp(operationtype,"mult")==0)
+   {
+     printf("%f \n",mult(x1,x2));
+   }
+   else if(strcmp(operationtype,"divi")==0)
+   {
+      printf("%f \n",divi(x1,x2));
+   }
+   else if (strcmp(operationtype,"addi")==0)
+   {
+     printf("%f \n",addi(x1,x2));
+   }
+   else if (strcmp(operationtype,"subt")==0)
+   {
+     printf("%f \n",subt(x1,x2));
+   }
+   else
+   {
+     printf("Invalid option. Try again with operation type.\n");
+   }
+
 
   statusmessage();
+   
   return(0);
 }
